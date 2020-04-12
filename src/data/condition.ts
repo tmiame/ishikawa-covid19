125368558610b14f7fc73aef54a834812edda563
tmiame <tmiame@gmail.com>
2020-04-13 03:38:12 +0900

    🦠

--- src/data/condition.ts
+++ src/data/condition.ts
@@ -1,6 +1,11 @@
 export const lastUpdateDateTime = '2020-04-10 23:59';
 const summaryData = [
   {
+    date: '2020-04-12 23:59',
+    hospitalized: 106,
+    discharged: 7,
+  },
+  {
     date: '2020-04-10 23:59',
     hospitalized: 85,
     discharged: 7,
--- src/data/news.ts
+++ src/data/news.ts
@@ -1,5 +1,13 @@
 const newsItems = [
   {
+    date: '2020-04-12 18:55',
+    url: 'https://www3.nhk.or.jp/news/html/20200412/k10012383041000.html',
+    urlArchive:
+      'https://web.archive.org/web/20200412174617/https://www3.nhk.or.jp/news/html/20200412/k10012383041000.html',
+    text: '石川県 新たに９人感染確認 集団感染の関係者２人含む 計113人 | NHKニュース',
+    showBrekingUpdate: true,
+  },
+  {
     date: '2020-04-11 19:00',
     url: 'https://www3.nhk.or.jp/news/html/20200411/k10012382061000.html',
     urlArchive:
